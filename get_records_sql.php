2595a2567358acdaf69ce70858445727480369a9
Masaki Sato <e.japonicus@gmail.com>
2019-07-05 17:40:01 +0900

    fix

--- get_records_sql.php
+++ get_records_sql.php
@@ -14,9 +14,11 @@ $peereval_records_sql = "SELECT * FROM {ispeereval_rubrics} WHERE peer_id = ?";
 $peereval_records = $DB->get_records_sql($peereval_records_sql, array($USER->id));
 $peer_name = array('Aさん', 'Bさん', 'Cさん', 'Dさん', 'Eさん', 'Fさん');
 
-var_dump($DB->get_records_sql($peereval_time, array($issetgoal->year, $issetgoal->subject)));
 
-echo "</br></br></br>";
+$test = "SELECT * FROM {ispeereval}";
+var_dump($DB->get_records_sql($test));
 
-var_dump($peereval_records); 
+// echo "</br></br></br>";
+
+// var_dump($peereval_records); 
 ?>
--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->component = 'mod_issetgoal';
-$plugin->version = 2019070508;
+$plugin->version = 2019070509;
 $plugin->release = 'v1.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
