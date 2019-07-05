cb5b8ffc940778ac008d401b6a84f88e651fa4db
Masaki Sato <e.japonicus@gmail.com>
2019-07-05 16:33:23 +0900

    20190705 v1.0 #fix

--- teachers_view.php
+++ teachers_view.php
@@ -5,25 +5,7 @@
 <!-- グラフのライブラリの読み込み -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
 
-<!-- <ul class="nav nav-tabs">
-  <li class="active"><a data-toggle="tab" href="#form">フォーム</a></li>
-  <li><a data-toggle="tab" href="#charts">全体の傾向</a></li>
-  <li><a data-toggle="tab" href="#list">ルーブリック登録一覧</a></li>
-</ul>
 
-<div class="tab-content">
-  <div id="form" class="tab-pane fade fade in active">
-    <?php require_once("{$CFG->dirroot}/mod/issetgoal/setgoal_form.php"); ?>
-  </div>
-
-  <div id="charts" class="tab-pane fade">
-    <?php require_once("{$CFG->dirroot}/mod/issetgoal/teachers_charts.php"); ?>
-  </div>
-
-  <div id="list" class="tab-pane fade">
-    <?php require_once("{$CFG->dirroot}/mod/issetgoal/teachers_list.php"); ?>
-  </div>
-</div> -->
 
 <!-- タブボタン部分 -->
   <ul class="nav nav-tabs">
